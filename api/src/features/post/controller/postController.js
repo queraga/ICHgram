fe04218a8554df32200c2d9daaf242d3374f0139
Lashkov Yevhen <evgene@icloud.com>
2026-07-13 19:21:31 +0200

    Implement post feed and likes

--- api/src/features/post/controller/postController.js
+++ api/src/features/post/controller/postController.js
@@ -26,3 +26,54 @@ export const createPost = async (req, res) => {
     });
   }
 };
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("author", "username fullName avatarUrl")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export const toggleLike = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const userId = req.user.userId;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+
+    const hasLiked = post.likes.some((likeId) => likeId.toString() === userId);
+
+    if (hasLiked) {
+      post.likes = post.likes.filter((likeId) => likeId.toString() !== userId);
+    } else {
+      post.likes.push(userId);
+    }
+
+    await post.save();
+
+    res.status(200).json({
+      message: hasLiked ? "Like removed" : "Post liked",
+      likesCount: post.likes.length,
+      likes: post.likes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- api/src/features/post/routes/postRoutes.js
+++ api/src/features/post/routes/postRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createPost } from "../controller/postController.js";
+
 import { authMiddleware } from "../../../shared/middleware/authMiddleware.js";
+import {
+  createPost,
+  getPosts,
+  toggleLike,
+} from "../controller/postController.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createPost);
+router.get("/", authMiddleware, getPosts);
+router.patch("/:postId/like", authMiddleware, toggleLike);
 
 export default router;
